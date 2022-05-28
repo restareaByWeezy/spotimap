8afa349580714e35df797f74e13710d4b2a7769c
restareaByWeezy <64529155+restareaByWeezy@users.noreply.github.com>
2022-05-28 14:52:38 +0900

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Routes.module.scss'
+
+const App = () => {
+  return (
+    <div className={styles.appWrapper}>
+      <div className={styles.app}>Main</div>
+    </div>
+  )
+}
+
+export default App
