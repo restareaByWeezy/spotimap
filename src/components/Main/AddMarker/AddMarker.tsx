ae0b335fe631cd594f2ecadba1752bc33f44f706
restareaByWeezy <64529155+restareaByWeezy@users.noreply.github.com>
2022-06-05 13:15:51 +0900

    lat, trackinfo validation

--- src/components/Main/AddMarker/AddMarker.tsx
+++ src/components/Main/AddMarker/AddMarker.tsx
@@ -4,11 +4,14 @@ import store from 'store'
 import { positionAtom, trackInfoAtom, markerInfoAtom } from 'states/atom'
 
 import styles from './AddMarker.module.scss'
+import { useEffect, useState } from 'react'
 
 const AddMarker = () => {
   const position = useRecoilValue(positionAtom)
   const trackInfo = useRecoilValue(trackInfoAtom)
   const [markerInfo, setMarkerInfo] = useRecoilState(markerInfoAtom)
+  const [hasTrack, setHasTrack] = useState(false)
+  const [hasLocation, setHasLocation] = useState(false)
 
   const handleClick = () => {
     if (position.lat !== 0 && Object.keys(trackInfo).length !== 0) {
@@ -18,18 +21,29 @@ const AddMarker = () => {
     }
   }
 
+  useEffect(() => {
+    trackInfo.artist && setHasTrack(true)
+    position.lat !== 0 && setHasLocation(true)
+  }, [trackInfo, position])
+
   return (
-    <div className={styles.container}>
-      <div className={styles.textWrap}>
-        <p className={styles.title}>Selected song:</p>
-        <p>{trackInfo.title} </p>
+    <>
+      <div className={styles.validationWrap}>
+        {!hasTrack && <p>Select your Song</p>}
+        {!hasLocation && <p>Specify your place</p>}
       </div>
-      <div className={styles.buttonWrap}>
-        <button type='button' onClick={handleClick}>
-          Add Marker
-        </button>
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.title}>Selected song:</p>
+          <p>{trackInfo.title} </p>
+        </div>
+        <div className={styles.buttonWrap}>
+          <button type='button' onClick={handleClick}>
+            Add Marker
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
